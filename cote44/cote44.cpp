8f2ba73c5d4050b7c8df7535dabc6b4fede415fc
ohne <ghdlr785@gmail.com>
2022-08-06 11:58:41 +0900

    초기 커밋.

---
+++ cote44/cote44.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    int temp = 1;
+    int count = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        temp += i;
+        count++;
+        if (temp > n)
+        {
+            break;
+        }
+        
+        if ((n - temp) % count == 0)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    solution(15);
+}
